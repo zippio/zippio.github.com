1414dd1990238de665fbcf5f44292ba57126d1a8
David Bohorquez <david.bohorquez@koombea.com>
2011-12-01 18:09:55 -0500

    added video

--- test/javascripts/global.js
+++ test/javascripts/global.js
@@ -1,11 +1,15 @@
 $(function(){
-  //$('.slider').flexslider();
-  /*$('.iphone').flexslider({
-    animation: 'slide',
-    after: function (){
-      
+  $('.modal').colorbox({
+    inline: true,
+    scrolling: false,
+    onComplete: function (){
+      $('.slider ul').trigger("pause", 1);
+      $('.iphone ul').trigger("pause", 1);
+      setTimeout(function (){
+        $.colorbox.resize();
+      }, 500);
     }
-  });*/
+  });
   
   $('.slider ul').carouFredSel({
     synchronise: '.iphone ul',
